89c68fbcb0122c9b8b0836fc5733457dbc1a4d00
mametochoko <mametochoko@gmail.com>
2019-06-19 01:17:40 +0900

    Create a base code

---
+++ README.md
@@ -0,0 +1,55 @@
+# Automate Operation
+
+Salesforceを対象に、操作を自動化
+
+## 概要 - Description
+ブラウザを起動せずにローカルで実行できるSOQLのクライアントツール。
+
+## 利用方法 - Usage
+### インストール - Install
+
+1. Node.jsをインストールします。
+1. 「npm install」を実行します。
+1. .envファイルを作成します。
+
+
+### 実行方法 - Run
+terminalで「node automation_tools.js」を実行します。
+
+### 操作方法 - Operation
+HELPを参照してください。
+
+## ヒント - Hints
+### 環境設定 - Env
+.envファイルは下記の項目を設定します。
+SFDC_ID={ユーザID}
+SFDC_PW={パスワード}
+SHEET_NAME=Sheet1
+
+### 制限事項 - Limitation
+既知の障害や機能改善はIssueを参照してください。
+
+### 操作例 - Examples Of Operation
+
+
+## 今後の方針 - Future Releases
+未定
+
+## 貢献 - Contribution
+1. Fork it
+2. Create your feature branch
+3. Commit your changes
+4. Push to the branch
+5. Create new Pull Request
+
+## ライセンス - License
+### Automate Operation ![MIT licensed][MIT]
+
+Copyright (c) 2017 chibitopoochan
+All rights reserved.
+
+
+https://github.com/mametochoko/AutomateOperation
+
+[MIT]: https://img.shields.io/badge/license-MIT-green.svg
+
---
+++ automation_tools.js
@@ -0,0 +1,150 @@
+/*
+ * Tool name "operation automate tools"
+ * How to Use:
+ * 1. Install Node.js
+ * 2. Execute node automation_tools.js
+ */
+
+// load modules
+const xlsx = require('xlsx');
+const puppeteer = require('puppeteer');
+const path = require('path');
+const fs = require('fs').promises;
+require('dotenv').config();
+
+// get .env informations
+const MY_ID = process.env.SFDC_ID;
+const MY_PW = process.env.SFDC_PW;
+const STEP_SHEET = process.env.SHEET_NAME;
+
+let headlessMode;
+
+class Step {
+  constructor(action, target, value, loop) {
+    this.action = action;
+    this.target = target;
+    this.value = value;
+    this.loop = loop;
+  }
+}
+
+async function loadExcelFile() {
+  const steps = [];
+
+  // search excel files
+  const filePath = path.join('.', 'input');
+  const files = await fs.readdir(filePath);
+  const fileName = files.pop();
+  console.log(`loading... ${filePath}\\${fileName}`);
+
+  // open excel file
+  const book = xlsx.readFile(path.join(filePath, fileName));
+  console.log('Excel opened');
+
+  // open sheet
+  const sheet = book.Sheets[STEP_SHEET];
+  console.log(`select ${STEP_SHEET} sheet`);
+
+  // get headless mode
+  headlessMode = sheet.H2.v;
+  console.log(`headless mode : ${headlessMode}`);
+
+  // define get cell function
+  const getValue = cell => ((typeof cell !== 'undefined' && cell.v !== 'undefined') ? cell.v : '');
+  const getActionCell = row => getValue(sheet[xlsx.utils.encode_cell({ c: 1, r: row })]);
+  const getTargetCell = row => getValue(sheet[xlsx.utils.encode_cell({ c: 2, r: row })]);
+  const getValueCell = row => getValue(sheet[xlsx.utils.encode_cell({ c: 3, r: row })]);
+  const getLoopCell = row => getValue(sheet[xlsx.utils.encode_cell({ c: 4, r: row })]);
+
+  // collect to step objects
+  let row = 3;
+  while (getActionCell(row) !== '') {
+    const step = new Step(
+      getActionCell(row),
+      getTargetCell(row),
+      getValueCell(row),
+      getLoopCell(row),
+    );
+    steps.push(step);
+    console.log(`step ${row} ${JSON.stringify(step)}`);
+
+    row += 1;
+  }
+  return steps;
+}
+
+/*
+ * Go to Action
+ */
+async function gotoAction(page, target) {
+  console.log(`GoTo : ${target}`);
+  await page.goto(target);
+}
+
+/*
+ * Type Action
+ */
+async function typeAction(page, target, value) {
+  const transID = param => (param === '$ID' ? MY_ID : param);
+  const transPW = param => (param === '$PW' ? MY_PW : param);
+  const typeValue = transID(transPW(value));
+  console.log(`Type : ${typeValue} to ${target} `);
+  await page.waitForSelector(target);
+  await page.type(target, typeValue);
+}
+
+/*
+ * Click Action
+ */
+async function clickAction(page, target) {
+  console.log(`Click : ${target}`);
+  await page.waitForSelector(target);
+  await page.click(target);
+}
+
+/*
+ * Tool boot
+ */
+(async () => {
+  // Load excel file
+  const steps = [];
+  await loadExcelFile().then(array => steps.push(...array));
+  console.log(steps);
+
+  const browser = await puppeteer.launch({
+    headless: headlessMode,
+    slowMo: 50,
+  });
+
+  const page = await browser.newPage();
+  if (headlessMode) {
+    await page.setViewport({ width: 1200, height: 800 });
+  }
+
+  console.log('Execute steps');
+  // eslint-disable-next-line no-restricted-syntax
+  for (let i = 0; i < steps.length; i += 1) {
+    const step = steps[i];
+    console.log(`Step : ${step.action}`);
+    switch (step.action) {
+      case 'GoTo':
+        // eslint-disable-next-line no-await-in-loop
+        await gotoAction(page, step.target);
+        break;
+      case 'Type':
+        // eslint-disable-next-line no-await-in-loop
+        await typeAction(page, step.target, step.value);
+        break;
+      case 'Click':
+        // eslint-disable-next-line no-await-in-loop
+        await clickAction(page, step.target);
+        break;
+      // TODO create an other steps
+      case 'DL':
+      case 'UL':
+      case 'Select':
+      default:
+        break;
+    }
+  }
+})();
